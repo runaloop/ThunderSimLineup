221bb23d27d7c3256a3b9637c11d93e62f4315df
AP <arkaha@gmail.com>
2020-07-11 09:38:13 +0800

    Del: Old code that is not used any more;

--- LocalDataConfigurator/src/main/java/com/catp/localdataconfigurator/main.kt
+++ LocalDataConfigurator/src/main/java/com/catp/localdataconfigurator/main.kt
@@ -1,7 +1,9 @@
 package com.catp.localdataconfigurator
 
+import com.dslplatform.json.DslJson
+import com.dslplatform.json.runtime.Settings
 import com.github.ajalt.clikt.core.subcommands
-
+val json = DslJson(Settings.withRuntime<Any>().includeServiceLoader().allowArrayFormat(true))
 @ExperimentalStdlibApi
 fun main(args: Array<String>) = MainParser()
     .subcommands(RegenerateXLSXFile(), GenerateJson(), ReadWTDump())
