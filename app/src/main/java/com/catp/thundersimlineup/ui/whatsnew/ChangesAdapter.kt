1f74c8c46d35beaaa9c4ab046cea366cb8a77dad
AP <arkaha@gmail.com>
2020-09-04 15:52:57 +0800

    Add: ChangesAdapter if BuildConfig.DEBUG active will print any of last recent events to a log;

--- app/src/main/java/com/catp/thundersimlineup/ui/whatsnew/ChangesAdapter.kt
+++ app/src/main/java/com/catp/thundersimlineup/ui/whatsnew/ChangesAdapter.kt
@@ -1,12 +1,13 @@
 package com.catp.thundersimlineup.ui.whatsnew
 
+import com.catp.thundersimlineup.BuildConfig
 import com.catp.thundersimlineup.data.db.entity.Change
 import com.catp.thundersimlineup.ui.list.ExpandableHeaderItem
 import com.catp.thundersimlineup.ui.list.HeaderColors
 import com.catp.thundersimlineup.ui.list.LogItem
+
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
-import org.threeten.bp.LocalDate
 
 
 class ChangesAdapter : FlexibleAdapter<AbstractFlexibleItem<*>>(null, null, false) {
@@ -27,6 +28,11 @@ class ChangesAdapter : FlexibleAdapter<AbstractFlexibleItem<*>>(null, null, fals
             }
             .sortedByDescending { it.first }
             .map { it.second }
+        if (BuildConfig.DEBUG) {
+            s.first().items.forEach {
+                println("ChangeAdapter: ${it.text}")
+            }
+        }
         updateDataSet(s, false)
     }
 
